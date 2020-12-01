ffbce0c30b55fdbc6f588175ce5ef64790ae029e
= <saleh.majeed8@gmail.com>
2020-12-01 17:18:04 +0530

    functions

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
 function setup() {
 	createCanvas(400, 400);
+	print('in the setup function');
 }
 
 function draw() {
 	background(220);
+
+	// rectMode(CENTER);
+	fill(0, 231, 0);
+	stroke(0, 0, 255);
+	strokeWeight(2);
+	rect(100, 20, 25, 75);
+
+	fill(255, 0, 0, 175);
+	noStroke();
+	ellipse(100, 50, 40, 30);
 }
