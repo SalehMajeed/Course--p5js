f83f90de48bb9a9ea7a0d7fc23a8d3fb8e4cf652
= <saleh.majeed8@gmail.com>
2020-12-02 15:15:45 +0530

    loops

--- sketch.js
+++ sketch.js
@@ -1,14 +1,25 @@
 function setup() {
 	createCanvas(400, 400);
-	background(100);
 }
 
 function draw() {
-	ellipse(40, 30, 43, 43);
+	background(100);
+
+	stroke(4);
+
+	fill(0);
+
+	var x = 0;
+
+	while (x < width) {
+		ellipse(x, 200, 40, 40);
+		x += 50;
+	}
+	for (let x = 25; x < 400; x += 50) ellipse(x, 300, 40, 40);
 }
 
-function mousePressed() {
-	background(255, 30, 10);
-	console.log('in');
-	fill(100);
-}
+// function mousePressed() {
+// 	background(255, 30, 10);
+// 	console.log('in');
+// 	fill(100);
+// }
