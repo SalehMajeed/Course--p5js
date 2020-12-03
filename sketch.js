3d93b14ca977629b817fd7efcb9413518d8dd069
= <saleh.majeed8@gmail.com>
2020-12-03 20:55:11 +0530

    different color

--- sketch.js
+++ sketch.js
@@ -1,25 +1,153 @@
+let _canvas = {
+	width: 600,
+	height: 400,
+	box: {
+		width: null,
+		height: null,
+	},
+	partition: {
+		x: 3,
+		y: 3,
+	},
+	bouncing_ball: [],
+	repeat_color: {},
+};
 function setup() {
-	createCanvas(400, 400);
+	createCanvas(_canvas.width, _canvas.height);
+	frameRate(60);
+	ellipseMode(RADIUS);
+	noStroke();
+	let balls = random(40, 50);
+	for (let i = 0; i < balls; i++) {
+		_canvas.bouncing_ball.push(new Bouncing_ball());
+	}
+
+	_canvas.box.width = width / _canvas.partition.x;
+	_canvas.box.height = height / _canvas.partition.y;
 }
 
 function draw() {
-	background(100);
+	background(200);
 
-	stroke(4);
-
-	fill(0);
-
-	var x = 0;
-
-	while (x < width) {
-		ellipse(x, 200, 40, 40);
-		x += 50;
+	for (const ball of _canvas.bouncing_ball) {
+		ball.move();
 	}
-	for (let x = 25; x < 400; x += 50) ellipse(x, 300, 40, 40);
 }
 
-function mousePressed() {
-	background(255, 30, 10);
-	console.log('in');
-	fill(100);
+class Bouncing_ball {
+	constructor() {
+		this.canvas_width = width;
+		this.canvas_height = height;
+		this.x = this.canvas_width / 2;
+		this.y = this.canvas_height / 2;
+		this.size = random(5, 30);
+		this.x_speed = random(0.01, 10);
+		this.y_speed = random(0.01, 10);
+		this.x_direction = random([1, -1]);
+		this.y_direction = random([1, -1]);
+		this.color = this.get_color();
+	}
+
+	get_color() {
+		let color = {
+			r: Math.floor(random(6, 255)),
+			g: Math.floor(random(6, 255)),
+			b: Math.floor(random(6, 255)),
+		};
+
+		let color_values = [];
+
+		let key = `r${color.r}g${color.g}b${color.b}`;
+
+		for (let i = -5; i <= 5; i++) {
+			let color_code = `r${color.r + i}g${color.g + i}b${color.b + i}`;
+
+			color_values.push(color_code);
+		}
+
+		if (key in _canvas.repeat_color) {
+			return this.get_color();
+		} else {
+			for (const val of color_values) {
+				_canvas.repeat_color[val] = '';
+			}
+			return color;
+		}
+	}
+
+	move() {
+		this.x = this.x + this.x_speed * this.x_direction;
+		this.y = this.y + this.y_speed * this.y_direction;
+
+		if (this.x <= _canvas.box.width - this.size && this.y <= _canvas.box.height - this.size) {
+			this.canvas_width = _canvas.box.width;
+			this.canvas_height = _canvas.box.height;
+
+			if (this.x >= this.canvas_width - this.size || this.x <= this.size) {
+				this.x_direction *= -1;
+			}
+
+			if (this.y >= this.canvas_height - this.size || this.y <= this.size) {
+				this.y_direction *= -1;
+			}
+		} else if (
+			this.x <= _canvas.box.width - this.size &&
+			this.y >= this.canvas_height - _canvas.box.height + this.size
+		) {
+			// bottom left
+			this.canvas_width = _canvas.box.width;
+			this.canvas_height = _canvas.box.height;
+
+			if (this.x <= this.size || this.x >= _canvas.box.width) {
+				this.x_direction *= -1;
+			}
+
+			if (this.y >= height - this.size || this.y <= height - _canvas.box.height) {
+				this.y_direction *= -1;
+			}
+		} else if (
+			this.x >= this.canvas_width - _canvas.box.width + this.size &&
+			this.y <= _canvas.box.height - this.size
+		) {
+			// top right
+			this.canvas_width = _canvas.box.width;
+			this.canvas_height = _canvas.box.height;
+
+			if (this.x >= width - this.size || this.x <= width - _canvas.box.width) {
+				this.x_direction *= -1;
+			}
+
+			if (this.y <= this.size) {
+				this.y_direction *= -1;
+			}
+		} else if (
+			this.x >= this.canvas_width - _canvas.box.width + this.size &&
+			this.y >= this.canvas_height - _canvas.box.height + this.size
+		) {
+			// bottom right
+			this.canvas_width = _canvas.box.width;
+			this.canvas_height = _canvas.box.height;
+
+			if (this.x >= width - this.size || this.x <= width - _canvas.box.width) {
+				this.x_direction *= -1;
+			}
+
+			if (this.y >= height - this.size || this.y <= height - _canvas.box.height) {
+				this.y_direction *= -1;
+			}
+		} else {
+			if (this.x >= this.canvas_width - this.size || this.x <= this.size) {
+				this.x_direction *= -1;
+			}
+
+			if (this.y >= this.canvas_height - this.size || this.y <= this.size) {
+				this.y_direction *= -1;
+			}
+		}
+
+		fill(this.color.r, this.color.g, this.color.b);
+		noStroke();
+
+		ellipse(this.x, this.y, this.size, this.size);
+	}
 }
